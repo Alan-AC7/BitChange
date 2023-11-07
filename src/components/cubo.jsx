22ec52ef250d88a2a0fca79820d39d9e73267874
Heinrich Ivan Elias Hernandez <fhernandaelias@gmail.com>
2023-11-07 15:35:20 -0600

    wallets info was added

--- src/components/cubo.jsx
+++ src/components/cubo.jsx
@@ -9,41 +9,54 @@ class Cubo extends React.Component {
             
             <div class="max-w-screen-xl mx-auto py-8 px-4 lg:py-16 lg:px-6">
     <div class="text-center mb-10">
-        <h2 class="text-4xl tracking-tight font-bold text-primary-800">Highlighted Features</h2>
+        <h2 class="text-4xl tracking-tight font-bold text-primary-800">CUBO+ Program</h2>
     </div>
 
     <div class="flex flex-col md:flex-row">
       
         <div class="mr-0 md:mr-8 mb-6 md:mb-0">
-            <img class="w-1/2 md:w-full mx-auto" src="https://placeholder.pics/svg/400" alt="can_help_banner"></img>
+            <img class="w-1/2 md:w-full mx-auto" src="https://cuboplus.dev/img/logo%20cubo+.png" alt="can_help_banner"></img>
         </div>
        
 
         <div class="flex-1 flex flex-col sm:flex-row flex-wrap -mb-4 -mx-2">
             <div class="w-full sm:w-1/2 mb-4 px-2 ">
                 <div class="h-full py-4 px-6 border border-green-500 border-t-0 border-l-0 rounded-br-xl">
-                    <h3 class="text-2xl font-bold text-md mb-6">Dynamic Personalization:</h3>
-                    <p class="text-sm">Our platform leverages user data and behavior to provide a highly personalized experience, with dynamic content and product recommendations that change in real-time.</p>
+                    <h3 class="text-2xl font-bold text-md mb-6">CUBO+</h3>
+                    <p class="text-sm">
+                        CUBO+ Educational Progrm is an iniative designed by and for a salvadorans, aimed at trainig the 
+                        upcoming generation of salvadoran programmers. 
+                    </p>
                 </div>
             </div>
             <div class="w-full sm:w-1/2 mb-4 px-2 ">
                 <div class="h-full py-4 px-6 border border-green-500 border-t-0 border-l-0 rounded-br-xl">
-                    <h3 class="text-2xl font-bold text-md mb-6">Mobile-Optimized Interface</h3>
-                    <p class="text-sm"> Our website is designed with a mobile-first approach, offering a seamless browsing experience across all devices, including smartphones and tablets.</p>
+                    <h3 class="text-2xl font-bold text-md mb-6">Preparatory period</h3>
+                    <p class="text-sm"> 
+                    The preparatory period, which ran from mid-April to early June, consisted of online sessions
+                    about the role of Bitcoin and its impact on society.
+
+                    </p>
                 </div>
             </div>
 
             <div class="w-full sm:w-1/2 mb-4 px-2 ">
                 <div class="h-full py-4 px-6 border border-green-500 border-t-0 border-l-0 rounded-br-xl">
-                    <h3 class="text-2xl font-bold text-md mb-6">24/7 Customer Support</h3>
-                    <p class="text-sm">ur U.S.-based customer support team is available around the clock to answer any questions, resolve any issues, and provide helpful solutions. Whether it's through email, phone, or live chat, we're always here to support you.</p>
+                    <h3 class="text-2xl font-bold text-md mb-6">On-site bootcamp</h3>
+                    <p class="text-sm">
+                        The on-site bootcamp took place the first two weeks of July focused on delving deeper into
+                        the LNP/BP technology.
+                    </p>
                 </div>
             </div>
 
             <div class="w-full sm:w-1/2 mb-4 px-2 ">
                 <div class="h-full py-4 px-6 border border-green-500 border-t-0 border-l-0 rounded-br-xl">
-                    <h3 class="text-2xl font-bold text-md mb-6">Secure Payment Processing</h3>
-                    <p class="text-sm">We use cutting-edge security measures to protect our customers' sensitive information and ensure the safety of all transactions made on our site.</p>
+                    <h3 class="text-2xl font-bold text-md mb-6">Tuscany bootcamp</h3>
+                    <p class="text-sm">
+                        During the first weeks of October the top candidates from the on-site bootcamp had
+                        the opportunity to travel to Europe to participate in the Tuscany Lightning Bootcamp.
+                    </p>
                 </div>
             </div>
 
--- src/components/wallets.jsx
+++ src/components/wallets.jsx
@@ -12,6 +12,8 @@ class Wallet extends React.Component {
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
  
  <div class="group cursor-pointer relative">
+  <a href="https://www.chivowallet.com/">
+  
  <h1>Chivo</h1>
  <br></br>
    <img
@@ -22,13 +24,15 @@ class Wallet extends React.Component {
    <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
    
    </div>
-  
+  </a>
  
  </div>
 
  
  <div class="group cursor-pointer relative">
- <h1>Blink Wallet</h1>
+  <a href="https://www.blink.sv/">
+  
+ <h1>Blink</h1>
  <br></br>
    <img
      src="https://play-lh.googleusercontent.com/a-fx1CdnEb8bswjDvvcxZYbf7ZtQ9wY4GsslocnuVIG8fU8qnv8Ke7s3qHBsQ6xkcQ"
@@ -36,11 +40,14 @@ class Wallet extends React.Component {
      class="w-full h-48 object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
    />
    <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
-    
+   
    </div>
+  </a>
+ 
  </div>
 
  <div class="group cursor-pointer relative">
+  <a href="https://breez.technology/">
  <h1>Breez Wallet</h1>
  <br></br>
    <img
@@ -51,10 +58,12 @@ class Wallet extends React.Component {
    <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
     
    </div>
+   </a>
  </div>
 
  <div class="group cursor-pointer relative">
- <h1>DitoBanx</h1>
+ <a href="https://www.walletofsatoshi.com/">
+ <h1>Wallet of Satoshi</h1>
  <br></br>
    <img
      src="https://financialit.net/sites/default/files/ditobanx.png"
@@ -64,14 +73,15 @@ class Wallet extends React.Component {
    <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
     
    </div>
+   </a>
    <br></br>
  </div>
 
  <div class="group cursor-pointer relative">
- 
+ <a href="https://phoenix.acinq.co/">
  <br></br>
    <img
-     src="https://financialit.net/sites/default/files/ditobanx.png"
+     src="https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/e8/30/40/e8304058-89c2-cc74-0828-f4289cf0ab60/AppIcon-Green-0-1x_U007emarketing-0-7-0-85-220-0.png/1200x630wa.png"
      alt="Image 1"
      class="w-full h-48 object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
    />
@@ -79,14 +89,14 @@ class Wallet extends React.Component {
     
    </div>
    <br></br>
-   <h1>DitoBanx</h1>
+   <h1>Phoenix</h1></a>
  </div>
 
  <div class="group cursor-pointer relative">
- 
+ <a href="https://bitkit.to/">
  <br></br>
    <img
-     src="https://financialit.net/sites/default/files/ditobanx.png"
+     src="https://play-lh.googleusercontent.com/N_yhsTGpnEwFYLDd1PmNiDIPI8yTKSn2dAMdLwLWWfDfDiRw2Dan6-O8srfI6efDY7c=w240-h480-rw"
      alt="Image 1"
      class="w-full h-48 object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
    />
@@ -94,14 +104,14 @@ class Wallet extends React.Component {
     
    </div>
    <br></br>
-   <h1>DitoBanx</h1>
+   <h1>Bitkit</h1></a>
  </div>
 
  <div class="group cursor-pointer relative">
- 
+ <a href="https://getalby.com/">
  <br></br>
    <img
-     src="https://financialit.net/sites/default/files/ditobanx.png"
+     src="https://getalby.com/assets/alby-logo-head-da6c4355b69a3baac3fc306d47741c9394a825e54905ef67c5dd029146b89edf.svg"
      alt="Image 1"
      class="w-full h-48 object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
    />
@@ -109,14 +119,14 @@ class Wallet extends React.Component {
     
    </div>
    <br></br>
-   <h1>DitoBanx</h1>
+   <h1>Alby</h1></a>
  </div>
 
  <div class="group cursor-pointer relative">
- 
+ <a href="https://blixtwallet.github.io/">
  <br></br>
    <img
-     src="https://financialit.net/sites/default/files/ditobanx.png"
+     src="https://play-lh.googleusercontent.com/W5RLJn9cStQV880Nkq9XYJ75eLq3HL3oevm8hVp9OmUZoF6UpLe3k3i2Qh1lri-LiQ"
      alt="Image 1"
      class="w-full h-48 object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
    />
@@ -124,7 +134,7 @@ class Wallet extends React.Component {
     
    </div>
    <br></br>
-   <h1>DitoBanx</h1>
+   <h1>Blixt</h1></a>
  </div>
 
  
