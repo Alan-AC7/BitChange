e85a94b6b76033d678028ccc580bea2dfa612d83
Alan-AC7 <xxprolegendzxx@gmail.com>
2023-11-08 15:08:40 -0600

    Final push

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ function App() {
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className="text-center lg:text-left">
       <h1 className="text-5xl font-bold text-black dark:text-white m-5">You received your change in BTC. Now what?</h1>
-      <p className="py-6 text-white m-5  text-black dark:text-white m-5 text-2xl">You can see BTC Map to see which businesses accept Bitcoin! </p>
+      <p className="py-6 text-black dark:text-white m-5 text-2xl">You can see BTC Map to see which businesses accept Bitcoin! </p>
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
       <div className="w-auto h-auto  flex items-center ">
