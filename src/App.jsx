9bf1c4398061dc3cf1d14cd7aa7c8ef2d625d12c
PatriciaElias <fhernandaelias@gmail.com>
2023-11-25 22:01:40 -0600

    video

--- src/App.jsx
+++ src/App.jsx
@@ -98,6 +98,31 @@ function App() {
 </div>
 </div>
 
+<div className="flex flex-col w-full border-opacity-50 p-10">
+ 
+  <div className="divider text-3xl text-[rgb(242,135,13)] font-bold"> BITCHANGE VIDEO </div>
+
+</div>
+
+<div className='p-10'>
+
+
+<div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold text-black dark:text-white m-5">Daily Change to Bitcoin</h1>
+      <p className="py-6 text-black dark:text-white m-5 text-2xl">BitChange provides Bitcoin change after fiat purchases</p>
+    </div>
+    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div className="w-auto h-auto  flex items-center ">
+<WebView source="https://btcmap.org/map#14/13.70136/-89.19408"  className="border-2 border-red-500"
+       />
+    </div>
+    </div>
+  </div>
+</div>
+</div>
+
 
 <div className="flex flex-col w-full border-opacity-50 p-10">
  
