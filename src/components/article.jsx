26c135d39495be386fa1bfc2766528ffb521b529
PatriciaElias <fhernandaelias@gmail.com>
2023-12-07 19:25:35 -0600

    why2

--- src/components/article.jsx
+++ src/components/article.jsx
@@ -1,61 +1,65 @@
-import '../index.css';
-import React from 'react';
+import '../index.css'
+import React from 'react'
 import { useTranslation } from 'react-i18next';
 
-function Article() {
-  const { t, i18n } = useTranslation();
+function Article () {
+  const {t,i18n} = useTranslation();
   return (
     <>
-      <div className='p-4 md:p-5'>
-        <div className="hero-content flex-col lg:flex-row-reverse items-center lg:items-start">
-          <img
-            src="https://u.today/sites/default/files/styles/1600x900/public/2023-03/31499.jpg"
-            className="max-w-full md:max-w-sm rounded-lg shadow-2xl mb-4 md:mb-0"
-            alt="Bitcoin Illustration"
-          />
-          <div className="pl-4">
-            <h1 className="text-4xl font-bold">
-              {t('article.start')}<span className="text-[rgb(242,135,13)]"> {t('article.start2')}</span>{' '}
-              {t('article.start3')}
-            </h1>
-            <br />
-            <p className="py-3 text-lg md:text-xl">{t('article.start4')}</p>
+   <div className='p-5'>
+  <div className="">
+    <div className="hero-content flex-col lg:flex-row-reverse items-center lg:items-start">
+      <img
+        src="https://u.today/sites/default/files/styles/1600x900/public/2023-03/31499.jpg"
+        className="max-w-sm rounded-lg shadow-2xl"
+      />
+      <div className="pl-4">
+        <h1 className="text-4xl font-bold">
+          {t('article.start')}<span className="text-[rgb(242,135,13)]"> {t('article.start2')}</span>{' '}
+          {t('article.start3')}
+        </h1>
+        <br />
+        <p className="py-5 text-xl">{t('article.start4')} </p>
 
-            <p className="py-3 text-lg md:text-xl">
-              <a
-                href="https://www.bitcoin.kn/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 hover:underline"
-              >
-                The Bitcoin Knowledge podcast
-              </a>
-            </p>
-            <p className="py-3 text-lg md:text-xl">
-              <a
-                href="https://podcasts.apple.com/es/podcast/bitcoin-en-espa%C3%B1ol/id1405877457"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 hover:underline"
-              >
-                Bitcoin Podcast
-              </a>
-            </p>
-            <p className="py-3 text-lg md:text-xl">
-              <a
-                href="https://bitcoinmagazine.com/articles"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 hover:underline"
-              >
-                Bitcoin Magazine
-              </a>
-            </p>
-          </div>
-        </div>
+        <p className="py-3 text-xl">
+          <a
+            href="https://www.bitcoin.kn/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:underline"
+          >
+            The Bitcoin Knowledge podcast
+          </a>
+        </p>
+        <p className="py-3 text-xl">
+          <a
+            href="https://podcasts.apple.com/es/podcast/bitcoin-en-espa%C3%B1ol/id1405877457"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:underline"
+          >
+            Bitcoin Podcast
+          </a>
+        </p>
+        <p className="py-3 text-xl">
+          <a
+            href="https://bitcoinmagazine.com/articles"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:underline"
+          >
+            Bitcoin Magazine
+          </a>
+        </p>
       </div>
+    </div>
+  </div>
+</div>
+    
+    
     </>
   );
-}
+};
 
 export default Article;
+
--- src/components/video.jsx
+++ src/components/video.jsx
@@ -1,44 +1,45 @@
-import '../index.css';
-import React from 'react';
+import '../index.css'
+import React from 'react'
 import { useTranslation } from 'react-i18next';
 
-function Video() {
-  const { t, i18n } = useTranslation();
+function Video () {
+  const {t,i18n} = useTranslation();
   return (
     <>
-      <div className='p-4 md:p-10'>
-        <div className="justify-center flex-1 px-4 py-6 mx-auto max-w-7xl lg:py-4 md:px-6">
-          <div className="hero-content flex-col lg:flex-row items-center justify-center">
-            <div className="orange-border p-4 rounded-lg">
-              <div className="text-top lg:text-left text-center lg:text-center">
-                <h1 className="text-3xl sm:text-5xl leading-normal font-extrabold tracking-tight font-bold">
-                  <span className="text-[rgb(242,135,13)]">{t('video.title1')}</span>
-                </h1>
-                <br />
-                <p className="text-lg lg:text-2xl mb-6 lg:mb-10">{t('video.text1')}</p>
-              </div>
+<div className='p-10'>
+  <div className="justify-center flex-1 px-4 py-6 mx-auto max-w-7xl lg:py-4 md:px-6">
+    <div className="hero-content flex-col lg:flex-row items-center justify-center">
+      <div className="orange-border p-4 rounded-lg">
+        <div className="text-top lg:text-left text-center lg:text-center">
+          <h1 className="text-3xl sm:text-5xl leading-normal font-extrabold tracking-tight font-bold">
+            <span className="text-[rgb(242,135,13)]">{t('video.title1')}</span>
+          </h1>
+          <br />
+          <p className="text-lg lg:text-2xl mb-6 lg:mb-10">{t('video.text1')}</p>
+        </div>
 
-              <div className="card flex-shrink-0 w-full max-w-3xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-                {/* Ajusta el ancho del contenedor del video y centra el contenido */}
-                <div className="w-full h-96 lg:h-auto flex items-center justify-center">
-                  <iframe
-                    title="YouTube Video"
-                    width="100%"
-                    height="100%"
-                    src="https://www.youtube.com/embed/L-Qhv8kLESY"
-                    frameBorder="0"
-                    allowFullScreen
-                  ></iframe>
-                </div>
-              </div>
-            </div>
+        <div className="card flex-shrink-0 w-full max-w-3xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+          {/* Ajusta el ancho del contenedor del video y centra el contenido */}
+          <div className="w-full h-96 flex items-center justify-center">
+            <iframe
+              title="YouTube Video"
+              width="100%"
+              height="100%"
+              src="https://www.youtube.com/embed/L-Qhv8kLESY"
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
           </div>
         </div>
       </div>
-    </>
+    </div>
+  </div>
+</div>
+
+</>
   );
-}
+};
+
 
 export default Video;
-
 
