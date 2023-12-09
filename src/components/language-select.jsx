b338f192cd23242620175b133984c5697bb4176e
PatriciaElias <fhernandaelias@gmail.com>
2023-12-08 20:10:30 -0600

    btcredeem

--- src/components/language-select.jsx
+++ src/components/language-select.jsx
@@ -10,11 +10,28 @@ function LanguageSelector() {
   };
 
   return (
-    <select onChange={(e) => changeLanguage(e.target.value)} value={i18n.language}>
-      <option value="en">English</option>
-      <option value="es">Español</option>
-      {/* Agrega más opciones según los idiomas que admitas */}
-    </select>
+    <div className="relative inline-block text-left">
+      <select
+        className="appearance-none bg-[rgb(242,135,13)] text-white py-1 px-2 pr-6 rounded leading-tight focus:outline-none focus:border-primary-color focus:border-2"
+        onChange={(e) => changeLanguage(e.target.value)}
+        value={i18n.language}
+      >
+        <option value="en">English</option>
+        <option value="es">Español</option>
+        {/* Agrega más opciones según los idiomas que admitas */}
+      </select>
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-white">
+        <svg
+          className="fill-current h-4 w-4"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <path
+            d="M10 12l-8 8h16l-8-8z"
+          />
+        </svg>
+      </div>
+    </div>
   );
 }
 
