cb4a4de796a74f7ead276acdd4c00641970854a7
PatriciaElias <fhernandaelias@gmail.com>
2023-12-16 21:12:03 -0600

    listbox

--- src/components/language-select.jsx
+++ src/components/language-select.jsx
@@ -10,34 +10,34 @@ function LanguageSelector() {
   };
 
   return (
-    <div className="relative inline-block text-left">
+    <div className="relative inline-flex items-center text-left">
       <select
-      className="appearance-none bg-[rgb(242,135,13)] text-white py-1 px-2 pr-6 rounded-md leading-tight focus:outline-none focus:border-primary-color focus:border-2"
-      onChange={(e) => changeLanguage(e.target.value)}
-      value={i18n.language}
+        className="appearance-none bg-[rgb(242,135,13)] text-white py-1 px-2 pr-6 rounded-md leading-tight focus:outline-none focus:border-primary-color focus:border-2 relative"
+        onChange={(e) => changeLanguage(e.target.value)}
+        value={i18n.language}
       >
-      <option value="en">English</option>
-      <option value="es">Español</option>
-      <option value="it">Italiano</option>
-      <option value="fr">Français</option>
-      <option value="ptbr">Português</option>
-      <option value="la">Lingua Latina</option>
-      <option value="de">Deutsch</option>
-      <option value="ko">한국어</option>
-      <option value="ja">日本語</option>
-      <option value="zh">中文</option>
-      <option value="uk">Український</option>
-      <option value="ru">Русский</option>
-      <option value="po">Polski</option>
-      <option value="ca">Català</option>
-      <option value="no">Norsk</option>
-      <option value="sv">Svensk</option>
-      <option value="tr">Türk</option>
-      {/* Agrega más opciones según los idiomas que admitas */}
+        <option value="en">English</option>
+        <option value="es">Español</option>
+        <option value="it">Italiano</option>
+        <option value="fr">Français</option>
+        <option value="ptbr">Português</option>
+        <option value="la">Lingua Latina</option>
+        <option value="de">Deutsch</option>
+        <option value="ko">한국어</option>
+        <option value="ja">日本語</option>
+        <option value="zh">中文</option>
+        <option value="uk">Український</option>
+        <option value="ru">Русский</option>
+        <option value="po">Polski</option>
+        <option value="ca">Català</option>
+        <option value="no">Norsk</option>
+        <option value="sv">Svensk</option>
+        <option value="tr">Türk</option>
+        {/* Agrega más opciones según los idiomas que admitas */}
       </select>
-      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-white">
+      <div className="pointer-events-none absolute bottom-0 right-6">
         <svg
-          className="fill-current h-4 w-4"
+          className="fill-current h-4 w-4 transform rotate-180 text-white"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
         >
